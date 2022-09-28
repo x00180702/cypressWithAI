3e9d0fdd8ada6ca526cd3f041edb5899ba36836c
John <x00180702@mytudublin.ie>
2022-09-28 20:10:18 +0100

    add depend

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,20 @@
 const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
+async function setupNodeEvents(on, config) {
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
 
+  on(
+    "file:preprocessor",
+    createBundler({
+      plugins: [createEsbuildPlugin.default(config)],
+    })
+  );
+
+  return config;
+}
 module.exports = defineConfig({
   projectId: 'gfeo6u',
   e2e: {
@@ -9,6 +23,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
-    specPattern: ['cypress/e2e/**/*.{feature,features}'],
+    specPattern: 'cypress/e2e/**/*.{feature,features}',
+    supportFile: false,
+    setupNodeEvents,
   },
-})
+});
