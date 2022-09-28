63ee8c8a8e4823f7dc6a37c5feafed86a0417c99
x00180702 <90932427+x00180702@users.noreply.github.com>
2022-09-28 13:17:08 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -19,11 +19,8 @@ async function setupNodeEvents(on, config) {
 module.exports = defineConfig({
   projectId: 'gfeo6u',
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
     specPattern: 'cypress/e2e/**/*.{feature,features}',
+    supportFile: false,
+    setupNodeEvents,
   },
-})
+});
