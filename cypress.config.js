ab5cf46eface51c509901b3028aa664ecf6d54e2
x00180702 <90932427+x00180702@users.noreply.github.com>
2022-09-28 13:14:06 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,20 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+
+async function setupNodeEvents(on, config) {
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+  on(
+    "file:preprocessor",
+    createBundler({
+      plugins: [createEsbuildPlugin.default(config)],
+    })
+  );
+
+  return config;
+}
 
 module.exports = defineConfig({
   projectId: 'gfeo6u',
