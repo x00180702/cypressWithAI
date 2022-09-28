a4b41d09c70372f94110a1efd7d96e56c5c391f7
x00180702 <90932427+x00180702@users.noreply.github.com>
2022-09-28 13:25:32 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,26 +1,13 @@
-const { defineConfig } = require("cypress");
-const createBundler = require("@badeball/cypress-esbuild-preprocessor");
-const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
-const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
-
-async function setupNodeEvents(on, config) {
-  await preprocessor.addCucumberPreprocessorPlugin(on, config);
-
-  on(
-    "file:preprocessor",
-    createBundler({
-      plugins: [createEsbuildPlugin.default(config)],
-    })
-  );
-
-  return config;
-}
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   projectId: 'gfeo6u',
   e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
     specPattern: 'cypress/e2e/**/*.{feature,features}',
-    supportFile: false,
-    setupNodeEvents,
   },
-});
+})
