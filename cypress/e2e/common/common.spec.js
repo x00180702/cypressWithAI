752da57373763db249f43998f4f9657cfe519584
x00180702 <90932427+x00180702@users.noreply.github.com>
2022-10-25 21:08:15 +0100

    Update common.spec.js

--- cypress/e2e/common/common.spec.js
+++ cypress/e2e/common/common.spec.js
@@ -49,6 +49,7 @@ And ("user clears Age field on the 'Blood Pressure' application", () => {
 
 When("user clicks on the submit button on the 'Blood Pressure' application", () => {
   homePage.getSubmitButton();
+  cy.percySnapshot('SubmitButton');
 });
 
 
